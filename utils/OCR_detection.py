7d1a2999db9608806f927f731b3a01877ebe046c
Bongo044152 <s33125433@gmail.com>
2025-01-30 19:25:05 +0800

    【添加功能】 - OCR 檢測是否存在特定文字
    【修復】
    - ROI 範圍錯誤
    - tesseract_path 檢查機制

--- utils/OCR_detection.py
+++ utils/OCR_detection.py
@@ -9,7 +9,10 @@ tesseract_path = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 if os.path.exists(tesseract_path):
     print(f"文件 '{tesseract_path}' 存在。")
 else:
-    print(f"文件 '{tesseract_path}' 不存在。")
+    print(f"請確保你使用的是 windows 系統或者你有下載過 tesseract-ocr")
+    print(f"tesseract-ocr 下載地址: https://github.com/UB-Mannheim/tesseract/wiki")
+    print(f"或者直接下載點擊此連結下載: https://github.com/tesseract-ocr/tesseract/releases/download/5.5.0/tesseract-ocr-w64-setup-5.5.0.20241111.exe")
+    exit(-1)
 
 pytesseract.pytesseract.tesseract_cmd = tesseract_path
 
@@ -26,11 +29,11 @@ class OCR_detection:
         roi_width = right_bottom[0] - left_top[0]
         roi_height = right_bottom[1] - left_top[1]
 
-        bac_w, bac_h = self.background_img.shape[:2]
+        bac_h, bac_w = self.background_img.shape[:2]
 
         if roi_width > bac_w or roi_height > bac_h:
             raise ValueError(f"ROI size must be smaller than background image size. "
-                            f"Required: width <= {bac_w}, height <= {bac_w}, "
+                            f"Required: width <= {bac_w}, height <= {bac_h}, "
                             f"but got: width = {roi_width}, height = {roi_height}")
         
         self.ROIleft = left_top[0]
@@ -46,8 +49,6 @@ class OCR_detection:
         # 二值化處理提高準確度
         _, binary = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
         data = pytesseract.image_to_data(binary, lang=language, output_type=pytesseract.Output.DICT)
-
-        print(data)
 
         # 過濾訊息
         del data['level']
@@ -92,26 +93,46 @@ class OCR_detection:
         width = right - left
         height = bottom - top
 
-        print(data)
-
         if self.ROI:
             left += self.ROIleft
             top += self.ROItop
 
         return left, top, width, height
     
+    def detect_text(self, target_str: str, conf_threshold: int = 70, language: str = 'chi_tra'):
+        gray = cv.cvtColor(self.background_img, cv.COLOR_BGR2GRAY)
+        _, binary = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
+
+        data = pytesseract.image_to_data(binary, lang=language, output_type=pytesseract.Output.DICT)
+        
+        total = len(target_str)
+        points = 0
+
+        # 檢查每個識別的單詞
+        for i in range(len(data['text'])):
+            word = data['text'][i].strip()
+            
+            # 檢查該單詞是否包含在目標字符串中
+            if word and any(char in target_str for char in word):
+                points += 1
+
+        if points / total > conf_threshold / 100:
+            return True
+        else:
+            return False
+    
 if __name__ == '__main__':
     from windowcapture import WindowCapture
-    from img_process import Img_helper
     import time
-    time.sleep(2)
+
+    time.sleep(1.5)
     wincap = WindowCapture()
     img = wincap.get_screenshot()
+    print(img.shape)
     ocr = OCR_detection(img)
-    left_top = (0, 0)
-    right_bottom = (img.shape[1]//4, img.shape[0]//4)
-    left, top, width, height = ocr.enable_ROI(left_top, right_bottom).get_text_xywh('資源回收筒')
-    right = left + width
-    buttom = top + height
-    img = cv.rectangle(img, (left, top), (right, buttom), (0, 255, 0), 2)
-    Img_helper.show_img(img)
+    left_top = (img.shape[1]//4 * 3, img.shape[0]//2)
+    right_bottom = (img.shape[1], img.shape[0])
+    if ocr.enable_ROI(left_top, right_bottom).detect_text('資源回收筒'):
+        print('find')
+    else:
+        print('not find')
